02b70d7c62dbfc667c19fa1d2429717553dc5e2b
Kiran Kumar S R <87469540+sai-kiran-sr@users.noreply.github.com>
2024-08-07 12:05:54 +0530

    Time: 1363 ms (44.12%), Space: 0B (100%) - LeetHub

---
+++ 0607-sales-person/0607-sales-person.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+SELECT s.name
+FROM
+    SalesPerson AS s
+    LEFT JOIN Orders USING (sales_id)
+    LEFT JOIN Company AS c USING (com_id)
+GROUP BY sales_id
+HAVING IFNULL(SUM(c.name = 'RED'), 0) = 0;
